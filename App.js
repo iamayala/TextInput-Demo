692bdd1ec278cb979f1ce82dc7cd7eda7c7b2a0e
iamayala <ndimayala@gmail.com>
2021-05-28 21:03:22 +0200

    editted app

--- App.js
+++ App.js
@@ -1,21 +1,106 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  TextInput,
+  Dimensions,
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      email: '',
+      activeUsername: false,
+      activeEmail: false,
+      showSuccess: false,
+      showFail: false,
+    };
+  }
+
+  onFocusUsername = (username) => {
+    this.setState({ activeUsername: true });
+    this.setState({ activeEmail: false });
+  };
+
+  onFocusEmail = (email) => {
+    this.setState({ activeEmail: true });
+    this.setState({ activeUsername: false });
+  };
+
+  onSubmit = () => {
+    if (this.state.username != '' || this.state.email != '') {
+      this.setState({ username: '' });
+      this.setState({ email: '' });
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          value={this.state.username}
+          onFocus={this.onFocusUsername}
+          onChangeText={(username) => this.setState({ username: username })}
+          style={[
+            styles.TextInput,
+            { borderColor: this.state.activeUsername ? '#ff5555' : '#e9eef5' },
+          ]}
+          placeholder="Username"
+        />
+
+        <TextInput
+          value={this.state.email}
+          onFocus={this.onFocusEmail}
+          onChangeText={(email) => this.setState({ email: email })}
+          style={[
+            styles.TextInput,
+            { borderColor: this.state.activeEmail ? '#ff5555' : '#e9eef5' },
+          ]}
+          keyboardType="email-address"
+          placeholder="Email"
+        />
+
+        <TouchableOpacity onPress={() => this.onSubmit()} style={styles.Button}>
+          <Text style={{ color: '#fff' }}>Continue</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
+const WIDTH = Dimensions.get('screen').width;
+const HEIGHT = Dimensions.get('screen').height;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingRight: WIDTH * 0.1,
+    paddingLeft: WIDTH * 0.1,
+    backgroundColor: 'white',
+  },
+  TextInput: {
+    backgroundColor: '#e9eef5',
+    width: WIDTH * 0.8,
+    height: 40,
+    borderRadius: 20,
+    paddingLeft: 15,
+    paddingRight: 15,
+    marginTop: 20,
+    borderWidth: 2,
+  },
+  Button: {
+    backgroundColor: '#ff5555',
+    width: WIDTH * 0.8,
+    height: 40,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
   },
 });
+
+export default App;
